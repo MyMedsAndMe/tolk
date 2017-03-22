bca938bd1bbfe5626e1b44c395bd8f2f35d4bb27
Alexey Blinov <nilcolor@gmail.com>
2017-03-22 15:25:50 +0300

    REP-3527 LMT: Ability to export language resource files in single file download

---
+++ app/controllers/tolk/export_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+module Tolk
+  class ExportController < Tolk::ApplicationController
+    ARCHIVE_NAME_PATTERN = "localisation-data-%s.tar.gz"
+
+    def show
+      datum = Tolk::Archive.gzip(Tolk::Archive.tar(Rails.root.join("config", "locales")))
+      send_data datum.string,
+                filename: format(ARCHIVE_NAME_PATTERN, Time.zone.now.iso8601.tr(":", "-")),
+                type: "application/octet-stream"
+    end
+  end
+end
--- lib/tolk.rb
+++ lib/tolk.rb
@@ -1,11 +1,13 @@
-require 'safe_yaml/load'
-require 'tolk/config'
-require 'tolk/engine'
-require 'tolk/sync'
-require 'tolk/import'
-require 'tolk/export'
-require 'tolk/yaml'
-require 'tolk/pagination'
+# frozen_string_literal: true
+require "safe_yaml/load"
+require "tolk/config"
+require "tolk/engine"
+require "tolk/sync"
+require "tolk/import"
+require "tolk/export"
+require "tolk/yaml"
+require "tolk/pagination"
+require "tolk/archive"
 
 module Tolk
   # Setup Tolk
